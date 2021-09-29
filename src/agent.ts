88845f774ebad8e8d3af7946126dff6e33c56485
Petra Jaros <peeja@peeja.com>
2021-09-29 10:03:46 -0400

    Load all OF data at boot

--- src/agent.ts
+++ src/agent.ts
@@ -1,54 +1,24 @@
 import watchOmniFocus from "./watchOmniFocus";
+import { upsert } from "./upsert";
 import MemDown from "memdown";
-import { clone, Subject, Update, uuid } from "@m-ld/m-ld";
+import { clone, uuid } from "@m-ld/m-ld";
 import { IoRemotes, MeldIoConfig } from "@m-ld/m-ld/dist/socket.io";
-import { isObject } from "lodash";
 
-const SINGLE_VALUED = new Set([
+const SINGLE_VALUED = [
   "http://peeja.com/purl/matters/omnifocus/o/added",
   "http://peeja.com/purl/matters/omnifocus/o/modified",
   "http://peeja.com/purl/matters/omnifocus/o/completed",
-]);
+];
 
 const config: MeldIoConfig = {
   "@id": uuid(),
   "@domain": "test.example.org",
   genesis: true,
   io: { uri: "http://localhost:4000" },
-  constraints: [...SINGLE_VALUED].map((property) => ({
+  constraints: SINGLE_VALUED.map((property) => ({
     "@type": "single-valued",
     property,
   })),
-};
-
-interface IdentifiedSubject extends Subject {
-  "@id": NonNullable<Subject["@id"]>;
-}
-const hasId = (subject: Subject): subject is IdentifiedSubject =>
-  !!subject["@id"];
-
-const update = (subjects: Subject[]): Update => {
-  // If any of the subjects have @ids and are setting properties that are
-  // single-valued, we need to delete any existing values when we add the new
-  // one.
-  const deletes = subjects.filter(hasId).flatMap((subject) =>
-    Object.keys(subject)
-      .filter((p) => SINGLE_VALUED.has(p))
-      .map((property) => ({
-        "@id": subject["@id"],
-        [property]: "?previous",
-      }))
-      .filter(isObject),
-  );
-
-  return {
-    "@delete": deletes,
-    "@insert": subjects,
-    // This weirdness is a workaround to ensure we do insert the new values,
-    // even if there's nothing to delete first.
-    // https://github.com/m-ld/m-ld-spec/issues/76#issuecomment-924669573
-    "@where": { "@union": [...deletes, { "@values": {} }] },
-  };
 };
 
 void (async () => {
@@ -56,7 +26,7 @@ void (async () => {
 
   watchOmniFocus().subscribe(async (data) => {
     try {
-      await meld.write(update(data as Subject[]));
+      await meld.write(upsert(data, SINGLE_VALUED));
     } catch (e) {
       console.error(JSON.stringify(e));
     }
--- src/watchOmniFocus.ts
+++ src/watchOmniFocus.ts
@@ -3,11 +3,10 @@ import { join } from "path";
 import fs from "fs/promises";
 
 import { parseOmniFocusXML } from "./parseOmniFocusXML";
-import { JSONValue } from "./types";
-import { isString } from "lodash";
 import JSZip from "jszip";
 import { watch } from "chokidar";
 import { concatMap, filter, fromEvent, Observable } from "rxjs";
+import { Subject } from "@m-ld/m-ld";
 
 const homeDir = process.env.HOME;
 
@@ -15,17 +14,17 @@ if (!homeDir) throw new Error("HOME not set! Can't find OmniFocus database.");
 
 const omniFocusDatabasePath = join(
   homeDir,
-  "Library/Containers/com.omnigroup.OmniFocus3/Data/Library/Application Support/OmniFocus/OmniFocus.ofocus",
+  "Library/Containers/com.omnigroup.OmniFocus3/Data/Library/Application Support/OmniFocus/OmniFocus.ofocus/*.zip",
 );
 
-export default function watchOmniFocus(): Observable<JSONValue> {
-  return fromEvent(
-    watch(omniFocusDatabasePath, {
-      ignoreInitial: true,
-    }),
-    "add",
-  ).pipe(
-    filter(isString),
+export default function watchOmniFocus(): Observable<Subject[]> {
+  const watcher = watch(omniFocusDatabasePath);
+
+  watcher.on("ready", () => {
+    console.log("All caught up.");
+  });
+
+  return fromEvent(watcher, "add", (path: string) => path).pipe(
     concatMap(async (path) => {
       const data = await fs.readFile(path);
       const zip = await JSZip.loadAsync(data);
