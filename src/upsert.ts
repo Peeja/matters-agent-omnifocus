854393a9447813ad3f67c2cce1f1951c8c40631e
Petra Jaros <peeja@peeja.com>
2021-10-30 12:45:25 -0400

    Document upsert()

--- src/upsert.ts
+++ src/upsert.ts
@@ -1,5 +1,21 @@
 import { Construct, MeldClone, StateProc, Subject } from "@m-ld/m-ld";
 
+// Only used for JSDoc.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { MeldStateMachine } from "@m-ld/m-ld";
+
+/**
+ * Creates a {@link StateProc} which will upsert the given data. That is, for
+ * every property in the {@link subject} data that is in
+ * {@link singleValuedProperties}, it will delete any existing value before
+ * inserting a new value. Non-single-valued properties in the subject data will
+ * be inserted normally.
+ * @param subjects The data to upsert into the state.
+ * @param singleValuedProperties A list of property names to consider
+ * "single-valued".
+ * @returns An upserting {@link StateProc} suitable for passing to
+ * {@link MeldStateMachine.write}.
+ */
 export const upsert = (
   subjects: Subject[],
   singleValuedProperties: string[],
